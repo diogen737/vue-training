64cd341eb78f19d62d924dbeaf95765d13ed2dc6
Oleg Musienko <diogen737@gmail.com>
2023-07-09 15:28:49 +0200

    feat: 🎸 initial, auto-generated vue app + tailwind

---
+++ src/views/AboutView.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { reactive } from 'vue'
+
+const state = reactive({ count: 0 })
+</script>
+
+<template>
+  <div class="about">
+    <!-- <h1>This is an about page</h1> -->
+    <button @click="state.count++">
+      {{ state.count }}
+    </button>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>

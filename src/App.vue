4214fe48defd2775a35abda1856a1dd4c88a734c
Oleg Musienko <diogen737@gmail.com>
2023-07-10 22:08:06 +0200

    fix: 🐛 remiove debug button

--- src/App.vue
+++ src/App.vue
@@ -20,18 +20,6 @@ function onModalClose(payload?: Movie) {
     movies.value.push(payload);
   }
 }
-
-function push() {
-  movies.value.push({
-    id: 111,
-    name: 'new movie',
-    description: 'qwerty',
-    genres: ['Drama'],
-    image: '',
-    inTheaters: false,
-    rating: 1,
-  })
-}
 </script>
 
 <template>
@@ -43,14 +31,6 @@ function push() {
         @click="newMovieModalOpen = true"
       >
         Add a movie
-      </button>
-
-      <button
-        type="button"
-        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-        @click="push"
-      >
-        Push movie
       </button>
     </div>
 

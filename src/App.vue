76962773ec5791902211f91189306c9ec11229e0
Oleg Musienko <diogen737@gmail.com>
2023-07-16 21:29:58 +0200

    refactor: 💡 move movie template to a separate component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
 import { computed, ref } from 'vue';
 
+import MovieItem from '@/components/MovieItem.vue';
 import ModalMovieUpsert from '@/components/ModalMovieUpsert.vue';
-import { StarIcon, TrashIcon, PlusIcon, PencilIcon } from '@heroicons/vue/24/solid';
-import { items } from './assets/movies.json';
+import { PlusIcon } from '@heroicons/vue/24/solid';
+import { items } from '@/assets/movies.json';
 
-import type { Movie } from './model/movie';
-import { generateMovieId } from './utils';
+import type { Movie } from '@/model/movie';
+import { generateMovieId } from '@/utils';
 
 /**
  * state
@@ -52,8 +53,8 @@ function onModalClose(payload?: Movie) {
 
 /**
  * movies CRUD
- * TODO: move to a store
  */
+
 function createMovie(movie: Movie) {
   movie.id = generateMovieId(movies.value);
   movies.value.push(movie);
@@ -119,80 +120,12 @@ function movieIndex(movie: Movie) {
 
     <div class="grid grid-cols-2 gap-5">
       <div v-for="movie in movies" :key="movie.id" class="col-span-full xl:col-span-1">
-        <div
-          class="relative md:max-h-56 overflow-hidden flex flex-col border rounded-lg shadow md:flex-row md:max-w- border-gray-700 bg-gray-800"
-        >
-          <div class="absolute left-2 top-2">
-            <StarIcon :class="['w-10', movie.rating ? 'text-yellow-300' : 'text-gray-300']" />
-            <span class="absolute w-full text-center top-1/2 -translate-y-1/2">
-              {{ movie.rating || '-' }}
-            </span>
-          </div>
-          <img
-            class="object-contain rounded-t-lg h-96 md:h-auto md:w-40 md:rounded-none md:rounded-l-lg"
-            :src="movie.image"
-            alt=""
-          />
-
-          <div class="flex-1 flex flex-col justify-between p-4 leading-normal">
-            <!-- name + delete button + rating -->
-            <div>
-              <div class="flex justify-between items-center">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                  {{ movie.name }}
-                </h5>
-
-                <div class="shrink-0">
-                  <button
-                    class="mr-2 text-sky-700 border border-sky-700 hover:bg-sky-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-sky-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-sky-500 dark:text-sky-500 dark:hover:text-white dark:focus:ring-sky-800 dark:hover:bg-sky-500"
-                    title="Edit movie"
-                    @click="openUpdateModal(movie)"
-                  >
-                    <PencilIcon class="w-4" />
-                  </button>
-                  <button
-                    class="text-red-700 border border-red-700 hover:bg-red-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:focus:ring-red-800 dark:hover:bg-red-500"
-                    title="Delete movie"
-                    @click="deleteMovie(movie)"
-                  >
-                    <TrashIcon class="w-4" />
-                  </button>
-                </div>
-              </div>
-
-              <div class="flex items-center space-x-1 mb-6">
-                <button
-                  v-for="idx in 5"
-                  :key="idx"
-                  :disabled="movie.rating === idx"
-                  class="disabled:cursor-not-allowed"
-                  @click="movie.rating = idx"
-                >
-                  <StarIcon
-                    class="w-4"
-                    :class="[idx > movie.rating ? 'text-gray-500' : 'text-yellow-500']"
-                  />
-                </button>
-              </div>
-            </div>
-
-            <!-- genres + description -->
-            <div>
-              <div class="flex items-center space-x-1 mb-2">
-                <span
-                  v-for="genre in movie.genres"
-                  :key="genre"
-                  class="bg-blue-100 text-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300"
-                >
-                  {{ genre }}
-                </span>
-              </div>
-              <p class="font-normal text-gray-700 dark:text-gray-400">
-                {{ movie.description }}
-              </p>
-            </div>
-          </div>
-        </div>
+        <MovieItem
+          :movie="movie"
+          @click-update="openUpdateModal(movie)"
+          @click-delete="deleteMovie(movie)"
+          @update-rating="movie.rating = $event"
+        />
       </div>
     </div>
   </div>
---
+++ src/components/MovieItem.vue
@@ -0,0 +1,89 @@
+<script setup lang="ts">
+import { StarIcon, TrashIcon, PencilIcon } from '@heroicons/vue/24/solid';
+
+import type { Movie } from '@/model/movie';
+
+defineProps<{ movie: Movie }>();
+defineEmits<{
+  (e: 'clickUpdate'): void;
+  (e: 'clickDelete'): void;
+  (e: 'updateRating', rating: number): void;
+}>();
+</script>
+
+<template>
+  <div
+    class="relative md:max-h-56 overflow-hidden flex flex-col border rounded-lg shadow md:flex-row md:max-w- border-gray-700 bg-gray-800"
+  >
+    <div class="absolute left-2 top-2">
+      <StarIcon :class="['w-10', movie.rating ? 'text-yellow-300' : 'text-gray-300']" />
+      <span class="absolute w-full text-center top-1/2 -translate-y-1/2">
+        {{ movie.rating || '-' }}
+      </span>
+    </div>
+    <img
+      class="object-contain rounded-t-lg h-96 md:h-auto md:w-40 md:rounded-none md:rounded-l-lg"
+      :src="movie.image"
+      alt=""
+    />
+
+    <div class="flex-1 flex flex-col justify-between p-4 leading-normal">
+      <!-- name + delete button + rating -->
+      <div>
+        <div class="flex justify-between items-center">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            {{ movie.name }}
+          </h5>
+
+          <div class="shrink-0">
+            <button
+              class="mr-2 text-sky-700 border border-sky-700 hover:bg-sky-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-sky-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-sky-500 dark:text-sky-500 dark:hover:text-white dark:focus:ring-sky-800 dark:hover:bg-sky-500"
+              title="Edit movie"
+              @click="$emit('clickUpdate')"
+            >
+              <PencilIcon class="w-4" />
+            </button>
+            <button
+              class="text-red-700 border border-red-700 hover:bg-red-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:focus:ring-red-800 dark:hover:bg-red-500"
+              title="Delete movie"
+              @click="$emit('clickDelete')"
+            >
+              <TrashIcon class="w-4" />
+            </button>
+          </div>
+        </div>
+
+        <div class="flex items-center space-x-1 mb-6">
+          <button
+            v-for="ratingIdx in 5"
+            :key="ratingIdx"
+            :disabled="movie.rating === ratingIdx"
+            class="disabled:cursor-not-allowed"
+            @click="$emit('updateRating', ratingIdx)"
+          >
+            <StarIcon
+              class="w-4"
+              :class="[ratingIdx > movie.rating ? 'text-gray-500' : 'text-yellow-500']"
+            />
+          </button>
+        </div>
+      </div>
+
+      <!-- genres + description -->
+      <div>
+        <div class="flex items-center space-x-1 mb-2">
+          <span
+            v-for="genre in movie.genres"
+            :key="genre"
+            class="bg-blue-100 text-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300"
+          >
+            {{ genre }}
+          </span>
+        </div>
+        <p class="font-normal text-gray-700 dark:text-gray-400">
+          {{ movie.description }}
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
